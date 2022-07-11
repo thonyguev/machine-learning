8654a327892f636e3051f889a3ac1177b267a199
Manuel Guevara <thonyguev-github@outlook.com>
2022-07-10 22:44:22 -0500

    Dependencias en el entorno Docker de Python 3.7.13

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python -m venv venv
+./venv/bin/pip install --upgrade pip
+./venv/bin/pip install -r requirements.txt
+./venv/bin/pip install jupyterlab
+./venv/bin/python -m jupyter lab --ip='0.0.0.0' --port=8888 --allow-root
